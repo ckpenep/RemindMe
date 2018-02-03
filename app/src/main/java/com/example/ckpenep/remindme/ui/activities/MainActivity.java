a9fa2b6b5e16b40c7cdf6a5693a9c95259162f4d
zappal <verter1988@yandex.ru>
2018-02-03 22:43:58 +0200

    Added fragment + NavigationView

--- app/src/main/java/com/example/ckpenep/remindme/ui/activities/MainActivity.java
+++ app/src/main/java/com/example/ckpenep/remindme/ui/activities/MainActivity.java
@@ -1,25 +1,40 @@
 package com.example.ckpenep.remindme.ui.activities;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
 import com.example.ckpenep.remindme.R;
+import com.example.ckpenep.remindme.ui.fragments.TaskListFragment;
 
 public class MainActivity extends AppCompatActivity {
 
     private Toolbar mToolbar;
-
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState)
+    {
+        setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        initToolbar();
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
+
+        if (fragment == null)
+        {
+            fragment = createFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragmentContainer, fragment)
+                    .commit();
+        }
+
+        initToolBar();
     }
 
-    private void initToolbar() {
+    private void initToolBar() {
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
         mToolbar.setTitle(R.string.app_name);
 
@@ -31,5 +46,9 @@ public class MainActivity extends AppCompatActivity {
         });
 
         mToolbar.inflateMenu(R.menu.menu);
+    }
+
+    private Fragment createFragment() {
+        return new TaskListFragment();
     }
 }
