88b4e32cba75d35723ec622ec3dc1055bc0cefbe
zappal <verter1988@yandex.ru>
2018-02-03 20:50:54 +0200

    Added TollBar and search button

---
+++ app/src/main/java/com/example/ckpenep/remindme/ui/activities/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.ckpenep.remindme.ui.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+import com.example.ckpenep.remindme.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initToolbar();
+    }
+
+    private void initToolbar() {
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mToolbar.setTitle(R.string.app_name);
+
+        mToolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                return false;
+            }
+        });
+
+        mToolbar.inflateMenu(R.menu.menu);
+    }
+}
